7d14b511e1de7c59c8835e40eb9be9f5ed9f5379
Satish Gunnu <satish.gunnu@gmail.com>
2009-05-01 00:30:58 -0400

    upgrading calendar gem. removed plugin and got new gem

--- config/environment.rb
+++ config/environment.rb
@@ -64,5 +64,6 @@ Rails::Initializer.run do |config|
 
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
+  config.gem "calendar_date_select"
 end
 CalendarDateSelect.format = :american
